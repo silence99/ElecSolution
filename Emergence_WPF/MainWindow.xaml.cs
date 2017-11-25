2d3d38080db0ca0fbb2fb0e9992f7a4821d85d3c
rock <liuff_yantai@126.com>
2017-11-25 18:57:23 +0800

    remove interactivity dll

--- Emergence_WPF/MainWindow.xaml.cs
+++ Emergence_WPF/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using Emergence_WPF.Comm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Emergence_WPF
 {
@@ -39,15 +27,15 @@ namespace Emergence_WPF
             Navigation = null;
 
             //Default set full screen
-            this.WindowState = System.Windows.WindowState.Normal;
-            this.WindowStyle = System.Windows.WindowStyle.SingleBorderWindow;
-            this.ResizeMode = System.Windows.ResizeMode.CanResize;
+            this.WindowState = WindowState.Normal;
+            this.WindowStyle = WindowStyle.SingleBorderWindow;
+            this.ResizeMode = ResizeMode.CanResize;
             //this.Topmost = true;
 
             this.Left = 0.0;
             this.Top = 0.0;
-            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
-            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight;    
+            this.Width = SystemParameters.PrimaryScreenWidth;
+            this.Height = SystemParameters.PrimaryScreenHeight;    
             //Maincontrol = new MainUserControl2();
             Maincontrol = new UserControl_MainPage();
             //Maincontrol.bind(this.ActualWidth, this.ActualHeight);
---
+++ Emergence_WPF/ViewModel/Video.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Emergence_WPF.ViewModel
+{
+    public class VideoUiModel: NotificationObject
+    {
+        public virtual string Uri { get; set; }
+        public virtual Guid EventID { get; set; }
+    }
+}
