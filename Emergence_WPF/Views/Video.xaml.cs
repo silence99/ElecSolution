f12849b738d38e33721ea4005bb3b2afcfdb2bab
rock <liuff_yantai@126.com>
2017-11-26 17:24:09 +0800

    add usercontrol 'Video'

--- Emergence_WPF/Views/Video.xaml.cs
+++ Emergence_WPF/Views/Video.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using Emergence_WPF.ViewModel;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Vlc.DotNet.Wpf;
 
 namespace Emergence_WPF.Views
 {
@@ -21,12 +14,112 @@ namespace Emergence_WPF.Views
     /// </summary>
     public partial class Video : UserControl
     {
-        protected NotificationObject UiModel = null;
         public Video()
         {
             InitializeComponent();
+            VideoPlay.MediaPlayer.VlcLibDirectoryNeeded += OnVlcControlNeedsLibDirectory;
+            VideoPlay.MediaPlayer.EndInit();
+
+            VideoPlay.MediaPlayer.Click += MediaPlayer_Click;
+        }
+
+        private void MediaPlayer_Click(object sender, EventArgs e)
+        {
+            Stop(VideoPlay);
+        }
+
+        private void OnVlcControlNeedsLibDirectory(object sender, Vlc.DotNet.Forms.VlcLibDirectoryNeededEventArgs e)
+        {
+            var currentAssembly = Assembly.GetEntryAssembly();
+            var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
+            if (currentDirectory == null)
+                return;
+
+            if (IntPtr.Size == 4)
+                e.VlcLibDirectory = new DirectoryInfo(Path.Combine(currentDirectory, @"lib\x86\"));
+            else
+                e.VlcLibDirectory = new DirectoryInfo(Path.Combine(currentDirectory, @"lib\x64\"));
+        }
+
+        public static readonly DependencyProperty UiModelProperty =
+           DependencyProperty.Register("UiModel", typeof(VideoUiModel),
+           typeof(Video),
+           new PropertyMetadata(new VideoUiModel(), new PropertyChangedCallback(OnViewModelChanged)));
+
+        public static readonly DependencyProperty VideoStatusProperty =
+           DependencyProperty.Register("State", typeof(VideoStatus),
+           typeof(Video),
+           new PropertyMetadata(VideoStatus.Stop, new PropertyChangedCallback(OnViewStateChanged)));
+
+        public VideoUiModel UiModel
+        {
+            get { return (VideoUiModel)GetValue(UiModelProperty); }
+
+            set
+            {
+                SetValue(UiModelProperty, value);
+                if (UiModel.ImageUri != null)
+                {
+                    var source = new BitmapImage(); ;
+                    source.BeginInit();
+
+                    source.UriSource = new Uri(Path.GetFullPath(UiModel.ImageUri));
+                    source.EndInit();
+                    VideoImage.Source = source;
+                }
+            }
+        }
+
+        public VideoStatus State
+        {
+            get { return (VideoStatus)GetValue(VideoStatusProperty); }
+            set { SetValue(VideoStatusProperty, value); }
+        }
+
+        public void OnImageClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 1)
+            {
+                var img = sender as Image;
+                img.Visibility = Visibility.Hidden;
+                VideoPlay.Visibility = Visibility.Visible;
+                Start(VideoPlay);
+            }
         }
 
 
+        static void OnViewModelChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+
+        }
+
+        static void OnViewStateChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            var videoCtl = sender as Video;
+            if (videoCtl != null)
+            {
+                videoCtl.Start(videoCtl.VideoPlay);
+            }
+        }
+
+        public void Start(VlcControl player)
+        {
+            player.MediaPlayer.Play(UiModel.Uri);
+        }
+
+        private void Stop(VlcControl player)
+        {
+            player.MediaPlayer.Stop();
+        }
+
+        private void Pause(VlcControl player)
+        {
+            player.MediaPlayer.Pause();
+        }
+
+        private void Resume(VlcControl player)
+        {
+            player.MediaPlayer.Play();
+        }
     }
 }
