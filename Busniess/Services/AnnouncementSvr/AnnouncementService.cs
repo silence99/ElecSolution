fda171bc8b2cfe41d396e8d4f107f669a57381ff
rock <liuff_yantai@126.com>
2017-12-24 22:59:01 +0800

    add announcement service

---
+++ Busniess/Services/AnnouncementSvr/AnnouncementService.cs
@@ -0,0 +1,157 @@
+﻿using Busniess.CommonControl;
+using Emergence.Common.Model;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Busniess.Services.AnnouncementSvr
+{
+	public class AnnouncementService
+	{
+		private ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+		public EmergencyHttpResponse<EmergencyHttpListResult<AnnouncementModel>> GetAnnounments(int pageIndex, int pageSize)
+		{
+			try
+			{
+				string serviceName = ConfigurationManager.AppSettings["getNoticesApi"] ?? "getNoticeList";
+				Dictionary<string, string> pairs = new Dictionary<string, string>()
+													{
+														{ "pageIndex", pageIndex.ToString() },
+														{ "pageSize", pageSize.ToString() }
+													};
+				var result = RequestControl.Request(serviceName, "GET", pairs);
+				if (result.StatusCode == 200)
+				{
+					Logger.DebugFormat("获取公告数据:{0}", result.Html);
+					return Utils.JSONHelper.ConvertToObject<EmergencyHttpResponse<EmergencyHttpListResult<AnnouncementModel>>>(result.Html);
+				}
+				else
+				{
+					throw new Exception(result.Html);
+				}
+			}
+			catch (Exception ex)
+			{
+				Logger.Error("获取公告数据异常", ex);
+				throw;
+			}
+		}
+
+		public bool CreateAnnouncement(string title, long time, string content)
+		{
+			string serviceName = ConfigurationManager.AppSettings["UpdateNoticeApi"] ?? "notice";
+			Dictionary<string, string> pairs = new Dictionary<string, string>()
+			{
+				{ "title", title },
+				{ "time", time.ToString() },
+				{ "content", content }
+			};
+
+			Logger.DebugFormat("创建公告 -- {0}", title);
+			var result = RequestControl.Request(serviceName, "POST", pairs);
+			if (result.StatusCode != 200)
+			{
+				Logger.WarnFormat("创建公告失败 -- {0}", title);
+				Logger.WarnFormat(result.Html);
+				return false;
+			}
+			else
+			{
+				var success = RequestControl.DefaultValide(result.Html);
+				if (success)
+				{
+					Logger.InfoFormat("创建公告成功 -- {0}", title);
+				}
+				else
+				{
+					Logger.WarnFormat("创建公告失败 -- {0}", title);
+					Logger.Warn(result.Html);
+				}
+
+				return success;
+			}
+		}
+
+		public bool UpdateMaterial(long id, string title, long time, string content)
+		{
+			string serviceName = ConfigurationManager.AppSettings["UpdateNoticeApi"] ?? "notice";
+			Dictionary<string, string> pairs = new Dictionary<string, string>()
+			{
+				{ "id", id.ToString() },
+				{ "title", title },
+				{ "time", time.ToString() },
+				{ "content", content }
+			};
+
+			Logger.DebugFormat("修改公告 -- {0}", title);
+			var result = RequestControl.Request(serviceName, "PUT", pairs);
+			if (result.StatusCode != 200)
+			{
+				Logger.WarnFormat("修改公告失败 -- {0}", title);
+				Logger.WarnFormat(result.Html);
+				return false;
+			}
+			else
+			{
+				var success = RequestControl.DefaultValide(result.Html);
+				if (success)
+				{
+					Logger.InfoFormat("修改公告成功 -- {0}", title);
+				}
+				else
+				{
+					Logger.WarnFormat("修改公告失败 -- {0}", title);
+					Logger.Warn(result.Html);
+				}
+
+				return success;
+			}
+		}
+
+		public bool DeleteAnncoucement(List<string> ids)
+		{
+			if (ids == null || ids.Count == 0)
+			{
+				Logger.Warn("公告ID列表为空，不能删除公告");
+				return true;
+			}
+
+			var idstring = string.Join(",", ids.ToArray());
+			string serviceName = ConfigurationManager.AppSettings["UpdateNoticeApi"] ?? "notice";
+			Dictionary<string, string> pairs = new Dictionary<string, string>()
+			{
+				{ "ids", idstring }
+			};
+
+			Logger.DebugFormat("删除公告 -- ID(s):{0}", ids);
+			var result = RequestControl.Request(serviceName, "DELETE", pairs);
+			if (result.StatusCode != 200)
+			{
+				Logger.WarnFormat("删除公告失败 -- ID(s):{0}", ids);
+				Logger.WarnFormat(result.Html);
+				return false;
+			}
+			else
+			{
+				var success = RequestControl.DefaultValide(result.Html);
+				if (success)
+				{
+					Logger.InfoFormat("删除公告成功 -- ID(s):{0}", ids);
+				}
+				else
+				{
+					Logger.WarnFormat("删除公告失败 -- ID(s):{0}", ids);
+					Logger.Warn(result.Html);
+				}
+
+				return success;
+			}
+		}
+	}
+}
---
+++ Emergence.Common/Model/AnnouncementModel.cs
@@ -0,0 +1,18 @@
+﻿using Framework;
+using Newtonsoft.Json;
+using System;
+
+namespace Emergence.Common.Model
+{
+	public class AnnouncementModel : NotificationObject
+	{
+		[JsonProperty("content")]
+		public string Content { get; set; }
+		[JsonProperty("id")]
+		public int ID { get; set; }
+		[JsonProperty("time")]
+		public DateTime Time { get; set; }
+		[JsonProperty("title")]
+		public string Title { get; set; }
+	}
+}
