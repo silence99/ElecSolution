25026f871eb2ccb5089911137336f3f5d395ff39
rock <liuff_yantai@126.com>
2018-01-06 17:41:08 +0800

    complete management page switch

--- Emergence_WPF/Views/Others/ManagementPage.xaml.cs
+++ Emergence_WPF/Views/Others/ManagementPage.xaml.cs
@@ -1,5 +1,5 @@
-﻿using Emergence_WPF.Comm;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Emergence_WPF.Views.Others
 {
@@ -11,10 +11,48 @@ namespace Emergence_WPF.Views.Others
 		public ManagementPage()
 		{
 			InitializeComponent();
+			InitPage();
 		}
 
-		private void Pager_OnPageChanged(object sender, PageChangedEventArg e)
+		private void InitPage()
 		{
+			ChangeStyle(Menu1);
+			ManagementPagesFrame.Navigate(new CameraManagementPage());
+		}
+
+		private void SwitchToCameroPage_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			ChangeStyle(sender as Label);
+			ManagementPagesFrame.Navigate(new CameraManagementPage());
+		}
+
+		private void SwitchToAnnouncementPage_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			ChangeStyle(sender as Label);
+			ManagementPagesFrame.Navigate(new AnnouncementManagementPage());
+		}
+
+		private void SwitchToArchivePage_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			ChangeStyle(sender as Label);
+			ManagementPagesFrame.Navigate(new MasterEventArchiveManagementPage());
+		}
+
+		private void SwitchToUpgradePage_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			ChangeStyle(sender as Label);
+			ManagementPagesFrame.Navigate(new UpgradeManagementPage());
+		}
+
+		private void ChangeStyle(Label label)
+		{
+			var brushWhite = new SolidColorBrush((Color)ColorConverter.ConvertFromString("White"));
+			var brushSeleted = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#006f69"));
+			Menu1.Background = brushWhite;
+			Menu2.Background = brushWhite;
+			Menu3.Background = brushWhite;
+			Menu4.Background = brushWhite;
+			label.Background = brushSeleted;
 		}
 	}
 }
