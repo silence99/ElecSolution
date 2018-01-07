0a3381a803e95caaafd9f846c28d20846a6cb334
rock <liuff_yantai@126.com>
2018-01-07 19:09:46 +0800

    add addresspicker control

---
+++ Emergence_WPF/Views/AddressPicker.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Emergence_WPF.Views
+{
+	/// <summary>
+	/// AddressPicker.xaml 的交互逻辑
+	/// </summary>
+	public partial class AddressPicker : UserControl
+	{
+		public AddressPicker()
+		{
+			InitializeComponent();
+		}
+
+		public event AddressGotHandler AddressGot;
+		public event EventHandler Close;
+
+		private void ButtonOK_Click(object sender, RoutedEventArgs e)
+		{
+			if (AddressGot != null)
+			{
+				var address = WebBrowser.InvokeScript("getAddress").ToString();
+				AddressPickerEventArgs args = new AddressPickerEventArgs()
+				{
+					Address = address
+				};
+				AddressGot(this, args);
+			}
+		}
+
+		private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+		{
+			Close?.Invoke(this, new EventArgs());
+		}
+	}
+
+	public class AddressPickerEventArgs : EventArgs
+	{
+		public string Address { get; set; }
+	}
+
+	public delegate void AddressGotHandler(object sender, AddressPickerEventArgs e);
+}
