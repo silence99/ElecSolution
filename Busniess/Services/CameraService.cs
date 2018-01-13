9bd39961e105a4b443fb50635b544ccd1a02ef74
rock <liuff_yantai@126.com>
2018-01-13 15:31:39 +0800

    add video pupup

---
+++ Busniess/Services/CameraService.cs
@@ -0,0 +1,69 @@
+﻿using Busniess.CommonControl;
+using Emergence.Common.Model;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Reflection;
+using Utils;
+
+namespace Busniess.Services
+{
+	public class CameraService
+	{
+		private ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+		public EmergencyHttpListResult<CameraModel> GetCamera(int pageIndex, int pageSize, double latitude, double longitude)
+		{
+			var response = GetCameraData(pageIndex, pageSize, latitude, longitude);
+			if (response == null || response.Code != 1)
+			{
+				return new EmergencyHttpListResult<CameraModel>()
+				{
+					Data = new CameraModel[0]
+				};
+			}
+			else
+			{
+				return response.Result;
+			}
+		}
+
+		private EmergencyHttpResponse<EmergencyHttpListResult<CameraModel>> GetCameraData(int pageIndex, int pageSize, double latitude, double longitude)
+		{
+			try
+			{
+				string serviceName = ConfigurationManager.AppSettings["mainEventCameraInfo"] ?? "mainEvent/getVideoList";
+				Dictionary<string, string> pairs = new Dictionary<string, string>()
+													{
+														{ "pageIndex", pageIndex.ToString() },
+														{ "pageSize", pageSize.ToString() },
+														{ "latitude", latitude.ToString() },
+														{ "longitude", longitude.ToString() }
+													};
+				var result = RequestControl.Request(serviceName, "GET", pairs);
+				if (result.StatusCode == 200)
+				{
+					Logger.DebugFormat("获取摄像头信息:（{0}, {1}）", latitude, longitude);
+					return JSONHelper.ConvertToObject<EmergencyHttpResponse<EmergencyHttpListResult<CameraModel>>>(result.Html);
+				}
+				else
+				{
+					Logger.ErrorFormat("获取摄像头信息异常:\r\n{0}", result.Html);
+					return new EmergencyHttpResponse<EmergencyHttpListResult<CameraModel>>()
+					{
+						Result = new EmergencyHttpListResult<CameraModel>()
+					};
+				}
+			}
+			catch (Exception ex)
+			{
+				Logger.Error("获取摄像头信息异常", ex);
+				return new EmergencyHttpResponse<EmergencyHttpListResult<CameraModel>>()
+				{
+					Result = new EmergencyHttpListResult<CameraModel>()
+				};
+			}
+		}
+	}
+}
---
+++ Emergence_WPF/Views/Event/SubEventPopup_VideoV2.xaml.cs
@@ -0,0 +1,129 @@
+﻿using Busniess.ViewModel;
+using Framework;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms.Integration;
+using Vlc.DotNet.Forms;
+
+namespace Emergence_WPF.Views.Event
+{
+	/// <summary>
+	/// SubEventPopup_VideoV2.xaml 的交互逻辑
+	/// </summary>
+	public partial class SubEventPopup_VideoV2 : Window
+	{
+		public SubEventPopupVideoViewModel ViewModel { get; set; }
+		public SubEventPopup_VideoV2()
+		{
+			InitializeComponent();
+			Vlc0.EndInit();
+			Vlc1.EndInit();
+			Vlc2.EndInit();
+			Vlc3.EndInit();
+			ViewModel = new SubEventPopupVideoViewModel().CreateAopProxy();
+			DataContext = ViewModel;
+		}
+
+		/// <summary>
+		/// called outside to init data
+		/// </summary>
+		/// <param name="latitude"></param>
+		/// <param name="longitude"></param>
+		public void SetCoordinate(double latitude, double longitude)
+		{
+			ViewModel.SetCoordinate(latitude, longitude);
+
+			//for test
+			//ViewModel.Cameras = new System.Collections.ObjectModel.ObservableCollection<Emergence.Common.Model.CameraModel>()
+			//{
+			//	new Emergence.Common.Model.CameraModel()
+			//	{
+			//		 Url = "http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi",
+			//		 VideoNumber = "001"
+			//	},
+			//	new Emergence.Common.Model.CameraModel()
+			//	{
+			//		 Url = "http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi",
+			//		 VideoNumber = "002"
+			//	},
+			//	new Emergence.Common.Model.CameraModel()
+			//	{
+			//		 Url = "http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi",
+			//		 VideoNumber = "003"
+			//	}
+			//};
+		}
+
+		private void MediaPlayer_TimeChanged(object sender, Vlc.DotNet.Core.VlcMediaPlayerTimeChangedEventArgs e)
+		{
+			var obj = sender as VlcControl;
+			obj.Audio.Volume = 0;
+		}
+
+		private void OnVlcControlNeedsLibDirectory(object sender, Vlc.DotNet.Forms.VlcLibDirectoryNeededEventArgs e)
+		{
+			var currentAssembly = Assembly.GetEntryAssembly();
+			var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
+			if (currentDirectory == null)
+				return;
+
+			if (IntPtr.Size == 4)
+				e.VlcLibDirectory = new DirectoryInfo(Path.Combine(currentDirectory, @"lib\x86\"));
+			else
+				e.VlcLibDirectory = new DirectoryInfo(Path.Combine(currentDirectory, @"lib\x64\"));
+		}
+
+		private void Video1SelectChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			var combox = sender as ComboBox;
+			if (string.IsNullOrEmpty(combox.SelectedValue.ToString()))
+			{
+				return;
+			}
+			Vlc0.Play(new Uri(combox.SelectedValue.ToString()));
+		}
+		private void Video2SelectChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			var combox = sender as ComboBox;
+			if (string.IsNullOrEmpty(combox.SelectedValue.ToString()))
+			{
+				return;
+			}
+			Vlc1.Play(new Uri(combox.SelectedValue.ToString()));
+		}
+		private void Video3SelectChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			var combox = sender as ComboBox;
+			if (string.IsNullOrEmpty(combox.SelectedValue.ToString()))
+			{
+				return;
+			}
+			Vlc2.Play(new Uri(combox.SelectedValue.ToString()));
+		}
+		private void Video4SelectChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			var combox = sender as ComboBox;
+			if (string.IsNullOrEmpty(combox.SelectedValue.ToString()))
+			{
+				return;
+			}
+			Vlc3.Play(new Uri(combox.SelectedValue.ToString()));
+		}
+
+		private void Window_Closed(object sender, EventArgs e)
+		{
+			Vlc0.Dispose();
+			Vlc1.Dispose();
+			Vlc2.Dispose();
+			Vlc3.Dispose();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
