0215735699be8a8d91837c92f9dd0d60f4749f59
rock <liuff_yantai@126.com>
2018-01-14 18:17:15 +0800

    binding master event archieve management service to page

---
+++ Busniess/ViewModel/MasterEventArchiveManagemenPageViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Busniess.Services;
+using Emergence.Common.Model;
+using Framework;
+using System.Collections.ObjectModel;
+using System.Configuration;
+
+namespace Busniess.ViewModel
+{
+	public class MasterEventArchiveManagemenPageViewModel : NotificationObject
+	{
+		public virtual int PageSize { get; set; }
+		public virtual int PageIndex { get; set; }
+		public virtual int TotalCount { get; set; }
+		public virtual int TotalPage { get; set; }
+
+		public virtual string SearchInfo { get; set; }
+		public virtual ObservableCollection<MasterEvent> Events { get; set; }
+
+		public MasterEventArchiveManagemenPageViewModel()
+		{
+			PageIndex = 1;
+			PageSize = GetPageSize();
+			TotalCount = 0;
+			TotalPage = 0;
+			SyncData();
+		}
+
+		private int GetPageSize()
+		{
+			var size = ConfigurationManager.AppSettings["PageSize"] ?? "10";
+			var t = 10;
+			if (!int.TryParse(size, out t))
+			{
+				t = 10;
+			}
+			return t;
+		}
+
+		public void SyncData()
+		{
+			var masterEventService = new MasterEventService();
+			var obj = this.IsAopWapper ? this : this.CreateAopProxy();
+			var data = masterEventService.GetAchievedMasterEvents(obj.PageIndex, obj.PageSize, obj.SearchInfo) ?? new EmergencyHttpListResult<MasterEvent>();
+			if (data.Data == null)
+			{
+				data.Data = new MasterEvent[0];
+			}
+
+			obj.Events = new ObservableCollection<MasterEvent>(data.Data);
+		}
+	}
+}
--- Emergence_WPF/Views/Others/MasterEventArchiveManagementPage.xaml.cs
+++ Emergence_WPF/Views/Others/MasterEventArchiveManagementPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Emergence_WPF.Comm;
+﻿using Busniess.ViewModel;
+using Emergence_WPF.Comm;
+using Framework;
 using System.Windows.Controls;
 
 namespace Emergence_WPF
@@ -8,13 +10,17 @@ namespace Emergence_WPF
 	/// </summary>
 	public partial class MasterEventArchiveManagementPage : Page
 	{
+		public MasterEventArchiveManagemenPageViewModel ViewModel { get; set; }
 		public MasterEventArchiveManagementPage()
 		{
 			InitializeComponent();
+			ViewModel = new MasterEventArchiveManagemenPageViewModel().CreateAopProxy();
+			DataContext = ViewModel;
 		}
 
 		private void Pager_OnPageChanged(object sender, PageChangedEventArg e)
 		{
+			ViewModel.SyncData();
 		}
 	}
 }
