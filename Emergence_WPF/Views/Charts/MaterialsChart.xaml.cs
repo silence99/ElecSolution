b9dee30f442ce6e51de383b4af711834e4059341
rock <liuff_yantai@126.com>
2018-01-14 19:33:20 +0800

    modify clustered column chart

--- Emergence_WPF/Views/Charts/MaterialsChart.xaml.cs
+++ Emergence_WPF/Views/Charts/MaterialsChart.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using Busniess.Services;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Emergence_WPF.Views.Charts
 {
@@ -7,10 +10,13 @@ namespace Emergence_WPF.Views.Charts
 	/// </summary>
 	public partial class MaterialsChart : UserControl
 	{
+		private MaterialsStatisticsService Service = new MaterialsStatisticsService();
 		public MaterialsChart()
 		{
 			InitializeComponent();
 		}
+
+
 
 		static MaterialsChart()
 		{
@@ -19,11 +25,44 @@ namespace Emergence_WPF.Views.Charts
 				var ctl = sender as MaterialsChart;
 				ctl.Chart.Width = ctl.Width;
 			}));
+
 			HeightProperty.OverrideMetadata(typeof(MaterialsChart), new System.Windows.FrameworkPropertyMetadata(100.0, (sender, e) =>
 			{
 				var ctl = sender as MaterialsChart;
 				ctl.Chart.Height = ctl.Height;
 			}));
+
+			BackgroundProperty.OverrideMetadata(typeof(MaterialsChart), new System.Windows.FrameworkPropertyMetadata(new SolidColorBrush((Color)ColorConverter.ConvertFromString("Black")), (sender, e) =>
+			{
+				var ctl = sender as MaterialsChart;
+				ctl.Chart.Background = ctl.Background;
+			}));
+
+			ForegroundProperty.OverrideMetadata(typeof(MaterialsChart), new System.Windows.FrameworkPropertyMetadata(new SolidColorBrush((Color)ColorConverter.ConvertFromString("White")), (sender, e) =>
+			{
+				var ctl = sender as MaterialsChart;
+				ctl.Chart.Foreground = ctl.Foreground;
+			}));
+		}
+
+		private void Chart_Loaded(object sender, System.Windows.RoutedEventArgs e)
+		{
+			var data = Service.GetMaterialsStatistics();
+			var names = new string[5];
+			var values = new double[5];
+			if (data != null)
+			{
+				double max = 1;
+				for (int i = 0; i < data.Length; i++)
+				{
+					names[i] = string.Format("{0}({1})", data[i].MaterialsName, data[i].TotalQuantity);
+					values[i] = data[i].TotalQuantity;
+					max = max >= data[i].TotalQuantity ? max : data[i].TotalQuantity;
+				}
+				values = values.Select(val => (100 * val / max)).ToArray();
+			}
+			Chart.ItemsSource = values;
+			Chart.ItemsSourceName = names;
 		}
 	}
 }
